c6b8f799cd5cd197293f2ecd2d4481c92c6537c9
StraetenMax <maxstraeten@yahoo.com>
2017-03-24 07:45:24 +0100

    finalisation de la boite de dialogue

--- src/com/philippe/BoiteDialog.java
+++ src/com/philippe/BoiteDialog.java
@@ -5,12 +5,14 @@ import javax.swing.JOptionPane;
 public class BoiteDialog {
 
 	public static void main(String[] args) {
-		String demande1 = JOptionPane.showInputDialog("Entrez un nombre ?");
+		String demande1 = JOptionPane.showInputDialog("Entrez un premier nombre ?");
 		int nbre1 = Integer.parseInt(demande1);
-		String demande2 =JOptionPane.showInputDialog("Entrez un nombre ?");
+		String demande2 =JOptionPane.showInputDialog("Entrez un deuxième nombre ?");
 		int nbre2 = Integer.parseInt(demande2);
 		
-		
+		String message = String.format("la somme est de %s ", (nbre1+nbre2));
+		//JOptionPane.showMessageDialog(null,"Bonjour "+nom);
+		JOptionPane.showMessageDialog(null, message);
 		
 	}
 
